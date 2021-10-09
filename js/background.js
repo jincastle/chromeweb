a7d584e34202bbe1d385e484177926dba2263669
jinsungPark <dksk0101@gmail.com>
2021-10-10 01:04:06 +0900

    weather_todo

--- js/background.js
+++ js/background.js
@@ -1,3 +1,4 @@
+
 const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg",];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
@@ -6,4 +7,9 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.background.appendChild(bgImage);
+document.body.appendChild(bgImage).classList.add("bdImage");
+
+
+
+
+
