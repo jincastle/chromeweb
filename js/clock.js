20e92958ec2ce09c1ada9ce935caefeffebe7750
jinsungPark <dksk0101@gmail.com>
2021-10-12 02:17:39 +0900

    10/12

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0"); //
+    const hours = String(date.getHours()).padStart(2,"0"); // 시간을현불러오고 문자열로 바꾼후 길이2에 빈공간은 0으로 채움
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    clock.innerText=`${hours}:${minutes}:${seconds}`;
+    clock.innerText=`${hours}:${minutes}:${seconds}`; // 시간 표현
 }
 
 getClock(); //페이지가 reload 된 후에 바로 시간을 표현해주기 위함
-setInterval(getClock, 1000);
+setInterval(getClock, 1000) //1초마다 함수 getClock 확인
--- js/login.js
+++ js/login.js
@@ -6,25 +6,24 @@ const username_key = "username";
 
 
 
-function onLoginSubmit(event) {
-    event.preventDefault();   // 브라우저의 기본 동작을 막는 함수
-    loginForm.classList.add(hidden_classname);
-    const username = loginInput.value;
-    localStorage.setItem(username_key, username);  // 후자가 변수
-    paintGreetings(username);  // username을 input으로부터 받음
+function onLoginSubmit(event) { // 함수 onLoginSubmit에 매개변수 event(실행? 작동?)을 넣음
+    event.preventDefault();   // event의 새로고침을 막음
+    loginForm.classList.add(hidden_classname); // loginform에 username 클래스 추가
+    const username = loginInput.value; // username = input입력값 
+    localStorage.setItem(username_key, username); // localStorage에 username 키와 value 저장
+    paintGreetings(username);  //함수 paintGreetings 실행
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(hidden_classname);
+    greeting.innerText = `Hello ${username}`; 
+    greeting.classList.remove(hidden_classname); // hidden클레스 제거 
 }
 
 const savedUsername = localStorage.getItem(username_key);
 
-if(savedUsername === null) {
-    loginForm.classList.remove(hidden_classname);
-    loginForm.addEventListener("submit", onLoginSubmit);
-} else {
-    paintGreetings(savedUsername);  // username을 local Storage로부터 받음
+if(savedUsername === null) { 
+    loginForm.classList.remove(hidden_classname); //login-form에 있는 hidden을 제거한다.
+    loginForm.addEventListener("submit", onLoginSubmit); // submit 사용?실행시 onLoginSubmit이라는 함수 실행
+} else { 
+    paintGreetings(savedUsername);  // 함수 paintGreetings에 매개변수 username을 받아서 실행
 }
-
